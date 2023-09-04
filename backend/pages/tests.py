aca79ab7fa6c27ea18e613df75baeb65025a2851
Kirill Duplyakin <kduplyakin@atlassian.com>
2023-09-04 20:30:54 +1000

    add about page stub with test

--- backend/pages/tests.py
+++ backend/pages/tests.py
@@ -13,10 +13,28 @@ class HomePageTests(SimpleTestCase):
         self.assertEqual(self.response.status_code, 200)
 
     def test_homepage_template(self):
-        self.assertTemplateUsed(self.response, "home.html")
+        self.assertTemplateUsed(self.response, "pages/home.html")
         self.assertContains(self.response, "Woohoo")
         self.assertNotContains(self.response, "Cats")
 
     def test_homepage_url_resolves_homepageview(self):  # new
         view = resolve("/")
+        self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)
+
+
+class AboutPageTests(SimpleTestCase):
+    def setUp(self) -> None:
+        url = reverse("about")
+        self.response = self.client.get(url)
+
+    def test_url_exists_at_correct_location(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_homepage_template(self):
+        self.assertTemplateUsed(self.response, "pages/about.html")
+        self.assertContains(self.response, "Yes dis dog")
+        self.assertNotContains(self.response, "Cats")
+
+    def test_homepage_url_resolves_homepageview(self):  # new
+        view = resolve("/about/")
         self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)
--- backend/pages/views.py
+++ backend/pages/views.py
@@ -2,5 +2,7 @@ from django.views.generic import TemplateView
 
 
 class HomePageView(TemplateView):
-    template_name = "home.html"
+    template_name = "pages/home.html"
     
+class AboutPageView(TemplateView):
+    template_name = "pages/about.html"
