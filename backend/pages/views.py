369789c780843a8816d1b80f56290b7bd517704c
Kirill Duplyakin <kduplyakin@atlassian.com>
2023-12-28 20:13:11 +1100

    update pipfile

--- backend/pages/views.py
+++ backend/pages/views.py
@@ -1,14 +1,13 @@
 from django.views.generic import TemplateView
 
-
 # TODO: clear this file and remove the import in views.py
-class HomePageView(TemplateView):
-    template_name = "pages/home.html"
+# class HomePageView(TemplateView):
+#     template_name = "pages/home.html"
 
 
-class AboutPageView(TemplateView):
-    template_name = "pages/about.html"
+# class AboutPageView(TemplateView):
+#     template_name = "pages/about.html"
 
 
-class TeaOfTheMonthPageView(TemplateView):
-    template_name = "pages/tea_of_the_month.html"
+# class TeaOfTheMonthPageView(TemplateView):
+#     template_name = "pages/tea_of_the_month.html"
