d1c8459013205d5f7f1ad00f786688a37f411ec3
StellaRabbit <jinzhaomddd@gmail.com>
2024-02-18 19:40:55 +0800

    add basic swiper and fix grid item expanding, this is the right commit

---
+++ backend/static/content/slider.js
@@ -0,0 +1,28 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 2,
+    spaceBetween:30,
+    // Responsive breakpoints
+    breakpoints: {
+        // touch devices
+        769: {
+          slidesPerView: 3,
+        },
+        // desktop
+        1024: {
+          slidesPerView: 4,
+        },
+        // widescreen
+        1216: {
+          slidesPerView: 5,
+        },
+      },
+  
+   
+    
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+   
+});
