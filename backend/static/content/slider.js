778ee2583d4d415e8124d2bc7c07656c286b6617
StellaRabbit <jinzhaomddd@gmail.com>
2024-03-12 18:25:41 +0800

    add pagination

--- backend/static/content/slider.js
+++ backend/static/content/slider.js
@@ -19,10 +19,10 @@ var swiper = new Swiper(".mySwiper", {
           slidesPerView: 5,
         },
       },
-  
-   
-    
-
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
